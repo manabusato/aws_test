5fe85390cb77095e76b169bd07b1f1332bbbf973
manabusato <manabu.sato@inte.co.jp>
2016-12-22 17:54:28 +0900

    Update common_variables.tf

--- terraform/common_variables.tf
+++ terraform/common_variables.tf
@@ -1,10 +1,5 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
 variable "region" {}
 variable "default_security_group" {}
-variable "lc_key_name" {}
 provider "aws" {
-  access_key = "${aws_access_key}"
-  secret_key = "${aws_secret_key}"
   region = "${var.region}"
 }
